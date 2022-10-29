dc99b12bd28c92a648bf8612917c8bd3a818b80d
SNUPI <joohaem@daum.net>
2022-10-29 12:10:29 +0900

    [ etc ] 이름 변경 & 주석

--- src/service/context/T2Context.tsx
+++ src/service/context/T2Context.tsx
@@ -3,19 +3,22 @@ import { createContext, useEffect, useState } from "react";
 
 import { InstanceContext } from "../../store/types/instanceContext.d";
 
-export const UnbuntuContext = createContext<InstanceContext>({
+export const T2Context = createContext<InstanceContext>({
   instances: new Set(),
   addInstance: () => {},
   removeInstance: () => {},
 });
 
-export default function UbuntuProvider(props: React.PropsWithChildren) {
+export default function T2Provider(props: React.PropsWithChildren) {
   const { children } = props;
 
   const [instances, setInstances] = useState<Set<string>>(new Set());
 
   useEffect(() => {
     // TODO :: 초기 API 통신하여 state 관리
+    // TODO :: 1. instance API
+    // TODO :: 2. Public/Private, ubuntu/centos 4가지 분류하여 setState
+    // TODO :: 3. 4가지 커스텀훅으로 분리하여 전달
   }, []);
 
   const addInstance = (instanceId: string) => {
@@ -27,7 +30,5 @@ export default function UbuntuProvider(props: React.PropsWithChildren) {
     setInstances(new Set(instances));
   };
 
-  return (
-    <UnbuntuContext.Provider value={{ instances, addInstance, removeInstance }}>{children}</UnbuntuContext.Provider>
-  );
+  return <T2Context.Provider value={{ instances, addInstance, removeInstance }}>{children}</T2Context.Provider>;
 }
--- src/service/hooks/useUbuntu.ts
+++ src/service/hooks/useUbuntu.ts
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 
-import { UnbuntuContext } from "../context/UbuntuContext";
+import { T2Context } from "../context/T2Context";
 
 export default function useLoader() {
-  const { instances, addInstance, removeInstance } = useContext(UnbuntuContext);
+  const { instances, addInstance, removeInstance } = useContext(T2Context);
 
   return {
     instances,
