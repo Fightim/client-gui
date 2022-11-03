bc63c4a8642a5f5c4c218f816a041d0e8bc7ea87
SNUPI <joohaem@daum.net>
2022-11-03 15:16:15 +0900

    [ feat ] add response dto type

---
+++ src/store/types/responseDto.d.ts
@@ -0,0 +1,47 @@
+interface Option {
+  name: string;
+}
+
+interface RdsOption extends Option {
+  masterUserName: string;
+}
+
+interface InstanceInformations {
+  id: string;
+  type: "t2.micro";
+  os: "UBUNTU" | "CENTOS";
+  tier: "WEBSERVER" | "WAS";
+  publicIp: string;
+  privateIp: string;
+  securityGroup: string[];
+  storageVolume: string;
+  storageType: "SSD" | "HDD";
+}
+
+interface LoadBalancerInformations {
+  id: string;
+  domain: string;
+  securityGroup: string[];
+  targetGroupInstances: string[];
+}
+
+interface RdsInformations {
+  endPoint: string;
+  port: number;
+  storage: string;
+}
+
+export interface InstanceResponseDto {
+  options: Option;
+  informations: InstanceInformations;
+}
+
+export interface LoadBalancerRsponseDto {
+  options: Option;
+  informations: LoadBalancerInformations;
+}
+
+export interface RdsRsponseDto {
+  options: RdsOptions;
+  informations: RdsInformations;
+}
