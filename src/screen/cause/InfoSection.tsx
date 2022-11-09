e4c7e5c4c06d475ec094a947d15bdc74b066743b
SNUPI <joohaem@daum.net>
2022-11-09 18:21:55 +0900

    docs: write InfoSection TODO

--- src/screen/cause/InfoSection.tsx
+++ src/screen/cause/InfoSection.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+// TODO ::
+// 1. react-query 틀잡기
+// 2. responseDto 옵셔널 타이핑
+// 3. 각 옵셔널에 맞는 UI 제작
+
 export default function InfoSection() {
   return <StInfoSection>right</StInfoSection>;
 }
