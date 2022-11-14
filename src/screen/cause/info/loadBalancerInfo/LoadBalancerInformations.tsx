1a77f136c4dca8321914016e5748d9ba04caf502
SNUPI <joohaem@daum.net>
2022-11-14 14:54:55 +0900

    feat:load-balancer options ui

--- src/screen/cause/info/loadBalancerInfo/LoadBalancerInformations.tsx
+++ src/screen/cause/info/loadBalancerInfo/LoadBalancerInformations.tsx
@@ -1,3 +1,26 @@
-export default function LoadBalancerInformations() {
-  return <div>LoadBalancerInformations</div>;
+import St from "../../@styled/infoInformations.styled";
+import ContentWrapper from "../ContentWrapper";
+
+// TODO :: any 삭제
+export default function LoadBalancerInformations(props: any) {
+  const { domain, securityGroup, targetGroupInstances } = props;
+
+  return (
+    <>
+      <St.Subject>인스턴스 정보</St.Subject>
+
+      <St.Title>도메인 이름</St.Title>
+      <ContentWrapper>{domain}</ContentWrapper>
+
+      <St.Title>보안 그룹</St.Title>
+      {securityGroup.map((security: string, idx: number) => (
+        <ContentWrapper key={idx}>{security}</ContentWrapper>
+      ))}
+
+      <St.Title>타겟 인스턴스 그룹</St.Title>
+      {targetGroupInstances.map((target: string, idx: number) => (
+        <ContentWrapper key={idx}>{target}</ContentWrapper>
+      ))}
+    </>
+  );
 }
--- src/screen/cause/info/loadBalancerInfo/LoadBalancerOptions.tsx
+++ src/screen/cause/info/loadBalancerInfo/LoadBalancerOptions.tsx
@@ -1,3 +1,18 @@
-export default function LoadBalancerOptions({ name }: { name: string }) {
-  return <div>LoadBalancerOptions</div>;
+import useInput from "../../../../service/hooks/useInput";
+import St from "../../@styled/infoOptions.styled";
+
+interface LoadBalancerOptionsProps {
+  name: string;
+}
+
+export default function LoadBalancerOptions({ name }: LoadBalancerOptionsProps) {
+  const { value, handleChangeValue } = useInput(name);
+
+  return (
+    <>
+      <St.Subject>설정 가능한 옵션</St.Subject>
+      <St.SubTitle>name</St.SubTitle>
+      <St.Input value={value} onChange={handleChangeValue} />
+    </>
+  );
 }
