b978aca0cf50cf0121a4a061e2db68818edb8af5
EXAMPLE1ST <koo6250@naver.com>
2022-11-18 21:17:11 +0900

    feat:화면 구현

--- src/screen/@page/Login.tsx
+++ src/screen/@page/Login.tsx
@@ -9,6 +9,58 @@
   - 
 */
 
+import React from "react";
+import styled from "styled-components";
+import "../login/Login.css";
+import applyimg from "../login/applyimg.png";
+
+const StyledInput=styled.input`
+height: 50px;
+width: 350px;
+border-radius: 6px;
+font-size: 14px;
+background-color:#F6F6FA;
+border:solid #DFDFDF;
+padding-left:20px;
+`;
+
+const StyledButton = styled.button`
+  height: 50px;
+  width: 200px;
+  background-color: #FF9A3E;
+  border-color: #FF9A3E;
+  font-color: #FFFFFF;
+  border-radius: 5px;
+  font-size:20px;
+  color:white;
+`;
+
+
 export default function Login() {
-  return <div>Login</div>;
-}
+  return (
+  <div>
+
+    <div className="out">
+    <a>email</a>
+    <br/>
+    <br/>
+    <StyledInput></StyledInput>
+    <br/>
+    <br/>
+    <a>password</a>
+    <br/>
+    <br/>
+    <StyledInput
+    ></StyledInput>
+    <br/>
+    <br/>
+    <br/>
+    <StyledButton><img
+                alt="apply"
+                src={applyimg}
+                width="20px"
+                height="20px"></img>&nbsp;확인</StyledButton>
+    </div>
+    </div>
+    );
+};
