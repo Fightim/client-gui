19396bf0c05db64b03ae4ae570f77dada4ee3cd4
EXAMPLE1ST <koo6250@naver.com>
2022-11-24 17:05:47 +0900

    feat: 여기까지

--- src/screen/@page/Login.jsx
+++ src/screen/@page/Login.jsx
@@ -12,103 +12,103 @@
 import styled from "styled-components";
 import "../login/Login.css";
 import applyimg from "../login/applyimg.png";
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
-import {loginUser} from "../login/loginUser.js";
+import { loginUser } from "../login/loginUser.js";
 
-const StyledInput=styled.input`
-height: 50px;
-width: 350px;
-border-radius: 6px;
-font-size: 14px;
-background-color:#F6F6FA;
-border:solid #DFDFDF;
-padding-left:20px;
+const StyledInput = styled.input`
+  height: 50px;
+  width: 350px;
+  border-radius: 6px;
+  font-size: 14px;
+  background-color:#F6F6FA;
+  border:solid #DFDFDF;
+  padding-left:20px;
 `;
 
 const StyledButton = styled.button`
-  height: 50px;
-  width: 200px;
-  background-color: #FF9A3E;
-  border-color: #FF9A3E;
-  font-color: #FFFFFF;
-  border-radius: 5px;
-  font-size:20px;
-  color:white;
+    height: 50px;
+    width: 200px;
+    background-color: #FF9A3E;
+    border-color: #FF9A3E;
+    font-color: #FFFFFF;
+    border-radius: 5px;
+    font-size:20px;
+    color:white;
 `;
 
 
 export default function Login() {
 
-  const dispatch=useDispatch();
- 
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
+  // const dispatch=useDispatch();
 
-  const onEmailHandler=(event)=>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
     setEmail(event.target.value);
   }
-  
-  const onPasswordHandler=(event)=>{
+
+  const onPasswordHandler = (event) => {
     setPassword(event.target.value);
   }
-  
-  
-  const submitHandler=(event)=>{
-    event.preventDefault(); //reload막기
-    console.log('email',email);
-    console.log('password',password);
 
-    let body={
-      email:email,
-      password:password,
+
+  const submitHandler = (event) => {
+    event.preventDefault(); //reload막기
+    console.log('email', email);
+    console.log('password', password);
+
+    let body = {
+      email: email,
+      password: password,
     }
-      dispatch(loginUser(body));    
-    }
-  
+    // dispatch(loginUser(body));   
+  }
+
 
   return (
     <>
-  <div>
+      <div>
 
-    <div className="out">
-      <form onSubmit={submitHandler}>
-    <a>email</a>
-    <br/>
-    <br/>
-    <StyledInput 
-    type="text" 
-    value={email} 
-    onChange={onEmailHandler}
-    ></StyledInput>
-    <br/>
-    <br/>
-    <a>password</a>
-    <br/>
-    <br/>
-    <StyledInput
-    type="password"
-    value={password}
-    onChange={onPasswordHandler}
-    ></StyledInput>
-    <br/>
-    <br/>
-    <br/>
-    <StyledButton
-    type="submit"
-    >
-      <img
-          alt="apply"
-          src={applyimg}
-          width="20px"
-          height="20px"></img>
-    &nbsp;확인</StyledButton>
-    <br/>
-    
-  </form>
-    </div>
-    </div>
+        <div className="out">
+          <form onSubmit={submitHandler}>
+            <a>email</a>
+            <br />
+            <br />
+            <StyledInput
+              type="text"
+              value={email}
+              onChange={onEmailHandler}
+            ></StyledInput>
+            <br />
+            <br />
+            <a>password</a>
+            <br />
+            <br />
+            <StyledInput
+              type="password"
+              value={password}
+              onChange={onPasswordHandler}
+            ></StyledInput>
+            <br />
+            <br />
+            <br />
+            <StyledButton
+              type="submit"
+            >
+              <img
+                alt="apply"
+                src={applyimg}
+                width="20px"
+                height="20px"></img>
+              &nbsp;확인</StyledButton>
+            <br />
+
+          </form>
+        </div>
+      </div>
     </>
-    );
+  );
 };
--- src/screen/login/loginUser.js
+++ src/screen/login/loginUser.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { LOGIN_USER } from './types.js';
 
-export function loginUser(dataToSubmit){
-    axios.post(`${import.meta.env.VITE_BASE_URL}/auth/login`,dataToSubmit)
-    .then(res=>{
-        console.log(res.data);
-        if(res.data.code===200){
-            console.log("로그인 완료");
-            //로그인 성공하면 페이지 이동하게
-            //token값 받아옴
-          }
+export function loginUser(dataToSubmit) {
+    axios.post(`${import.meta.env.VITE_BASE_URL}/auth/login`, dataToSubmit)
+        .then(res => {
+            console.log(res.data);
+            if (res.data.code === 200) {
+                console.log("로그인 완료");
+                //로그인 성공하면 페이지 이동하게
+                //token값 받아옴
+            }
         });
 
     // return{
