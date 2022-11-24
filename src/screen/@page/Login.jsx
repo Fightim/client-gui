e337aab6a6cfbd7852c82cb347d357c4aa34704f
SNUPI <joohaem@daum.net>
2022-11-24 20:07:01 +0900

    fix: re modify login page

--- src/screen/@page/Login.jsx
+++ src/screen/@page/Login.jsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import applyimg from "../login/applyimg.png";
-import { createUser, postLoginUser } from "../login/loginUser";
 import { StyledButton, StyledInput } from "../login/styled";
+import { createUser, postLoginUser } from "../login/userAPI";
 
 export default function Login() {
   const navigate = useNavigate();
