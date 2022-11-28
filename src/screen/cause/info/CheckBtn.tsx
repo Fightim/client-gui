939ffc08dbb54a7bdbae79c945520a32d653a32e
SNUPI <joohaem@daum.net>
2022-11-28 13:14:38 +0900

    feat: modify instance check button

--- src/screen/cause/info/CheckBtn.tsx
+++ src/screen/cause/info/CheckBtn.tsx
@@ -2,9 +2,19 @@ import { IcApply } from "../../../store/assets";
 import theme from "../../../store/style/theme";
 import { BtnWrapper } from "../@styled/button.styled";
 
-export default function CheckBtn({ hyperLink }: { hyperLink: string }) {
+interface CheckBtnProps {
+  hyperLink: string;
+}
+
+export default function CheckBtn(props: CheckBtnProps) {
+  const { hyperLink } = props;
+
+  const handleClickLinkBtn = () => {
+    window.location.href = hyperLink;
+  };
+
   return (
-    <BtnWrapper href={hyperLink} target="_blank" bgcolor={theme.colors.blue200}>
+    <BtnWrapper type="button" onClick={handleClickLinkBtn} bgcolor={theme.colors.blue200}>
       <IcApply />
       인스턴스 확인
     </BtnWrapper>
