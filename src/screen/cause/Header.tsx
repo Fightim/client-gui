1393b233c288321a35166ed263aee281baa21040
SNUPI <joohaem@daum.net>
2022-11-28 15:19:42 +0900

    feat: add logo

--- src/screen/cause/Header.tsx
+++ src/screen/cause/Header.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { ImgLogo } from "../../store/assets";
 import CompleteButton from "./header/CompleteButton";
 
 export default function Header() {
   return (
     <StHeaderContainer>
-      <StLogo>svg</StLogo>
+      <StLogo src={ImgLogo} alt="logo" />
       <StLinksContainer>
         <Link to="/key">AWS 연동</Link>
         <Link to="/cause">아키텍쳐 설계</Link>
@@ -27,9 +28,12 @@ const StHeaderContainer = styled.header`
   box-shadow: 4px 4px 4px rgba(78, 78, 78, 0.25);
 `;
 
-const StLogo = styled.div`
+const StLogo = styled.img`
   position: absolute;
   left: 2rem;
+
+  width: 14.8rem;
+  height: 3.6rem;
 `;
 
 const StLinksContainer = styled.div`
--- src/store/assets/index.ts
+++ src/store/assets/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as IcArrowBlue } from "./arrow_blue.svg";
 export { ReactComponent as IcDelete } from "./delete.svg";
 export { ReactComponent as IcDeleteAll } from "./delete_all.svg";
 export { default as GifLoader } from "./loader.gif";
+export { default as ImgLogo } from "./logo_148_36.png";
