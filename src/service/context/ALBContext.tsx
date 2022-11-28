a31cd9e0cc6c56597c5e19609b6dc62611d67982
SNUPI <joohaem@daum.net>
2022-11-28 17:52:31 +0900

    etc: 불필요 로직 제거

--- src/service/context/ALBContext.tsx
+++ src/service/context/ALBContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { createContext, useEffect } from "react";
+import { createContext } from "react";
 
 import { LoadBalancerContext } from "../../store/types/instanceContext.d";
 import useALBData from "../hooks/instanceContext/instanceData/useALBData";
@@ -14,10 +14,6 @@ export default function ALBProvider(props: React.PropsWithChildren) {
   const { children } = props;
 
   const { instances, addInstance, removeInstance } = useALBData();
-
-  useEffect(() => {
-    // TODO :: 초기 API 통신하여 state 관리
-  }, []);
 
   return <ALBContext.Provider value={{ instances, addInstance, removeInstance }}>{children}</ALBContext.Provider>;
 }
