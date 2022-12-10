aef5dbe26b077100ccbace402fabbe5866fcf168
SNUPI <joohaem@daum.net>
2022-12-10 13:10:43 +0900

    feat: rds axios function

--- src/story/api/rds.ts
+++ src/story/api/rds.ts
@@ -5,14 +5,26 @@ export const PATH = {
   rds: "/rds"
 }
 
-export const getRDSOption = async (id: string) => {
-  const { data } = await client.get(`${PATH.rds}/${id}`);
+export const getRDS = async () => {
+  const { data } = await client.get(PATH.rds);
 
   return data;
 };
 
-export const createRDS = async (rds: CreateRdsDto) => {
-  const { data } = await client.post(PATH.rds, rds);
+export const getRDSOption = async (rdsId: string) => {
+  const { data } = await client.get(`${PATH.rds}/${rdsId}`);
+
+  return data;
+};
+
+export const createRDS = async (rdses: CreateRdsDto[]) => {
+  const { data } = await client.post(PATH.rds, rdses);
+
+  return data;
+};
+
+export const deleteRDS = async (rdsId: string) => {
+  const { data } = await client.delete(`${PATH.rds}/${rdsId}`);
 
   return data;
 };
