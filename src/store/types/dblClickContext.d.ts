0ad60ce84acd4fdcb69577b72d6a18c82119268d
SNUPI <joohaem@daum.net>
2022-12-12 00:37:14 +0900

    feat: reset after removing instance

--- src/store/types/dblClickContext.d.ts
+++ src/store/types/dblClickContext.d.ts
@@ -1,8 +1,9 @@
-import { InstanceIconType, InstanceIconTypeWithNull } from "./instanceIcon.d";
+import { InstanceIconTypeWithNull } from "./instanceIcon.d";
 
 export interface IDblClickContext {
   instanceId: string;
   instanceType: InstanceIconTypeWithNull;
   handleInstanceId: (_id: string) => void;
-  handleInstanceType: (_currentType: InstanceIconType) => void;
+  handleInstanceType: (_currentType: InstanceIconTypeWithNull) => void;
+  resetInstance: () => void;
 }
