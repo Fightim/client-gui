651d6f21d1451818448c96ca6a6e7cf48b939f8c
SNUPI <joohaem@daum.net>
2022-12-12 01:18:26 +0900

    feat: axios interceptor for access token

--- src/story/api/index.ts
+++ src/story/api/index.ts
@@ -1,10 +1,25 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 const LOCAL_STORAGE_KEY = "user-token";
 
+const getAccessToken = () => localStorage.getItem(LOCAL_STORAGE_KEY) ? `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY)}` : "";
+
 export const client = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY)}`,
-  },
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY)}`,
+  // },
 });
+
+client.interceptors.request.use(
+  function (config: AxiosRequestConfig) {
+    const accessToken = getAccessToken();
+    if (accessToken && config.headers) {
+      config.headers['Authorization'] = accessToken;
+    }
+    return config;
+  },
+  // TODO :: request ERROR 처리
+  // function (error) {
+  // }
+)
